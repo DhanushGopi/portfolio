64e94d65482499dbb5b48066e8fcaeaff8458971
DhanushGopi <g.dhanushgopi1509@gmail.com>
2025-10-10 23:40:03 +0530

    update: the header font

--- src/sections/HeroSec.jsx
+++ src/sections/HeroSec.jsx
@@ -9,8 +9,8 @@ export default function HeroSec() {
     return(
         <div className="herosec">
              {/* <p className="herosec-subheader">Design or Development? No Worries, I do both!</p> */}
-            <p className="herosec-header herosec-header1"><i>“ Wild enough to</i> <span className="hightext">Imagine </span><i>and</i></p>
-            <p className="herosec-header herosec-header2"><span className="hightext1">Skilled</span> <i>enough to build ”</i></p>
+            <p className="herosec-header herosec-header1">“ WILD ENOUGH TO <span className="hightext">IMAGINE </span>AND</p>
+            <p className="herosec-header herosec-header2"><span className="hightext1">SKILLED</span> ENOUGH TO BUILD ”</p>
             <p className="herosec-subheader">Design or Development? No Worries, I do both!</p>
             <SuperBtn className="super-btn__herosec" funcact={handleExternalLink} btntext="Let's Connect"/>
         </div>
